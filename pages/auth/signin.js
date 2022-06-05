2133bd3cc224b691cf87b6fbb7a929505fd62ee5
shubham <shubhamrawatktw@gmail.com>
2022-06-05 11:42:59 +0530

    complete sign in page

---
+++ pages/auth/signin.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {getProviders,signIn} from 'next-auth/react'
+import Header from "../../components/Header"
+
+export default function signin({providers}) {
+  return (
+    <div>
+     <Header/>
+     <div className="flex justify-center space-x-7 mt-20">
+         <img className='hidden object-cover rotate-6 md:inline-flex md:w-60 ' src="https://like4like.com/img/hero-x750.png" alt="insta" />
+         <div className="">
+             {Object.values(providers).map(provider =>{
+                 return <div className="flex flex-col items-center" key={provider.name}>
+                     <img className='w-40 object-cover mb-6' src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202005/social-1834010_1920__1_.png?O0d57AlZjyqseIYIHsdyoE8B_ttFJjGh&size=770:433" alt="image-insta" />
+                     <button className='bg-red-400 rounded-lg p-3 text-white hover:bg-red-500'  onClick={()=>signIn(provider.id, {callbackUrl:"/"})}>Sign in with {provider.name}</button>
+                 </div>
+             })}
+         </div>
+     </div>
+    </div>
+  )
+}
+
+
+export async function getServerSideProps(context){
+    const providers = await getProviders()
+    return {
+        props:{providers}
+    }
+}
